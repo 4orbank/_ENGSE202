d4ae8f3ddee2a7098ff702c3b15c84f7098e925a
4orbank <tanakhan@gmail.com>
2025-12-20 16:57:52 +0700

    Lab4_6

--- Lab4/Lab4_6.java
+++ Lab4/Lab4_6.java
@@ -1,5 +1,52 @@
 package Lab4;
+import java.util.Scanner;
+
+class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point move(int dx, int dy) {
+        return new Point(x + dx, y + dy);
+    }
+
+    public void displayInfo() {
+        System.out.println("(" + x + ", " + y + ")");
+    }
+}
 
 public class Lab4_6 {
+    public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int startX = input.nextInt();
+        int startY = input.nextInt();
+        int moveX = input.nextInt();
+        int moveY = input.nextInt();
+
+        Point p1 = new Point(startX, startY);
+        Point p2 = p1.move(moveX, moveY);
+        Point p3 = p2.move(moveX, moveY);
+
+        p1.displayInfo();
+
+        if (moveX != 0 || moveY != 0) {
+            p2.displayInfo();
+            p3.displayInfo();
+        }
+    }
 }
